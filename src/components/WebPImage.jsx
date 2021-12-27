bb8fb6b766d13874c716d790584db029dae83363
Stephen <stephen@domainmoney.com>
2021-12-26 22:12:38 -0500

    More support

--- src/components/WebPImage.jsx
+++ src/components/WebPImage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 export const webpImage = (props, img) => {
-  const srcSplit = props.src.split('.');
-  const cacheString = srcSplit[1].split('?');
-  const webpSrc = `${srcSplit[0]}.webp${cacheString[1] ? `?${cacheString[1]}` : ''}`;
+	const srcSplit = props.src.split('.');
+	const cacheString = srcSplit[1].split('?');
+	const webpSrc = `${srcSplit[0]}.webp${
+		cacheString[1] ? `?${cacheString[1]}` : ''
+	}`;
 
-  return (
-    <picture>
-      <source srcSet={webpSrc} type='image/webp' />
-      {img}
-    </picture>
-  );
+	return (
+		<picture>
+			<source srcSet={webpSrc} type="image/webp" />
+			{img}
+		</picture>
+	);
 };
